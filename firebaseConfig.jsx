1888e02447061024069164c6a5eb3e20bbcb5496
Mahfujul-hoque <mdmahfujulhoque123@gmail.com>
2024-06-06 01:28:29 +0800

    add-firebase-upload

---
+++ firebaseConfig.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtWn37Jhz5xvdUE8k0-5pgKdu0HR88oI8",
+  authDomain: "file-sharing-d17f6.firebaseapp.com",
+  projectId: "file-sharing-d17f6",
+  storageBucket: "file-sharing-d17f6.appspot.com",
+  messagingSenderId: "526096945860",
+  appId: "1:526096945860:web:b457327c8dfc9da7bfef91"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
